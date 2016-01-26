aa6bc481529022bb878fcd76903bb0ccb93d7048
Peter Amstutz <peter.amstutz@curoverse.com>
2016-01-26 16:17:26 -0500

    Fix schema salad pin.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='cwltool',
           'rdflib >= 4.2.0',
           'rdflib-jsonld >= 0.3.0',
           'shellescape',
-          'schema_salad == 1.5.20160126164443'
+          'schema_salad == 1.5.20160126190004'
         ],
       test_suite='tests',
       tests_require=[],
