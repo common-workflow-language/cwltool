a3fda08eb14d026b703ae1701def334f795194a0
Peter Amstutz <peter.amstutz@curoverse.com>
2016-03-16 16:40:54 -0400

    Remove ez_setup and bump schema salad dependency again.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name='cwltool',
           'rdflib >= 4.2.0',
           'rdflib-jsonld >= 0.3.0',
           'shellescape',
-          'schema_salad == 1.7.20160316150109'
+          'schema_salad == 1.7.20160316203940'
         ],
       test_suite='tests',
       tests_require=[],
