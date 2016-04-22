2c1731f959b11c9761bd959dd9602e3ae3306392
Michael R. Crusoe <michael.crusoe@gmail.com>
2016-04-22 10:47:13 +0200

    add convenient entry point for cwltool (#66)
    * add convenient entry point for cwltool
    * clarify that this is not recommended

---
+++ cwltool.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Convienance entry point for cwltool.
+
+This can be used instead of the recommended method of `./setup.py install`
+or `./setup.py develop` and then using the generated `cwltool` executable.
+"""
+
+import sys
+from cwltool import main
+
+if __name__ == "__main__":
+    sys.exit(main.main(sys.argv[1:]))
