f8243bfe2df901f56965bf55647524c78716d9e6
Peter Amstutz <peter.amstutz@curoverse.com>
2016-04-28 09:23:08 -0400

    Remove unused files.

--- tests/cache-test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python ../cwltool/main.py --debug --cache-intermediate-output --tmpdir-prefix=/Users/spanglry/Code/cwltool/tests/ --tmp-outdir-prefix=/Users/spanglry/Code/cwltool/tests/ revsort-workflow.cwl --input revsort-workflow.cwl
