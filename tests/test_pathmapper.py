0b9086e5cc9b474a064b1a81df752966f143b914
Peter Amstutz <tetron@interreality.org>
2017-04-28 10:36:36 -0400

    Strip trailing slash (#368)
    * Strip trailing slashes on path component of location URIs.

--- tests/test_pathmapper.py
+++ tests/test_pathmapper.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cwltool.pathmapper import PathMapper
+from cwltool.pathmapper import PathMapper, normalizeFilesDirs
 
 
 class TestPathMapper(unittest.TestCase):
@@ -12,3 +12,17 @@ class TestPathMapper(unittest.TestCase):
 
         a = SubPathMapper([], '', '', "new")
         self.assertTrue(a.new, "new")
+
+    def test_strip_trailing(self):
+        d = {
+                "class": "Directory",
+                "location": "/foo/bar/"
+            }
+        normalizeFilesDirs(d)
+        self.assertEqual(
+            {
+                "class": "Directory",
+                "location": "/foo/bar",
+                "basename": "bar"
+            },
+            d)
