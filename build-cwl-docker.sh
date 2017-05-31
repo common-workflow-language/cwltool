64f39f805d3134bec8c5e8a7ba86b3b5df2f117c
Tomoya Tanjo <ttanjo@gmail.com>
2017-05-31 17:10:51 +0900

    Build docker image with tags if it is released version

--- build-cwl-docker.sh
+++ build-cwl-docker.sh
@@ -2,3 +2,10 @@
 set -e
 docker build --file=cwltool_module.Dockerfile --tag=commonworkflowlanguage/cwltool_module .
 docker build --file=cwltool.Dockerfile --tag=commonworkflowlanguage/cwltool .
+
+version=$(git describe --tags)
+echo $version | grep -vq '\-' >& /dev/null
+if [ $? -eq 0 ];then
+    docker tag commonworkflowlanguage/cwltool_module commonworkflowlanguage/cwltool_module:$version
+    docker tag commonworkflowlanguage/cwltool commonworkflowlanguage/cwltool:$version
+fi
