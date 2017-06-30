c43a32054ca48f1ba058f1b8ffbe68c783c143dc
Michael R. Crusoe <michael.crusoe@gmail.com>
2017-06-30 06:23:33 -0700

    fix tests to work with unreleased schema_salad

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,5 +1,7 @@
 import unittest
 
+from six.moves import urllib
+
 import schema_salad.main
 import schema_salad.ref_resolver
 import schema_salad.schema
@@ -31,6 +33,16 @@ outputs: []
                     return True
                 else:
                     return False
+
+	    def urljoin(self, base, url):
+                urlsp = urllib.parse.urlsplit(url)
+                if urlsp.scheme:
+                    return url
+                basesp = urllib.parse.urlsplit(base)
+
+                if basesp.scheme == "keep":
+                    return base + "/" + url
+                return urllib.parse.urljoin(base, url)
 
         def test_resolver(d, a):
             return "baz:bar/" + a
