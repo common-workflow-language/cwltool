050899711e75cf06092acd1943a61e3a429d59f5
kapilkd13 <kapilkd13@gmail.com>
2017-07-05 05:22:14 +0530

    correcting position of default container resolution into hints. Previously default container was not propogated to workflow steps due to wrong position

--- cwltool/main.py
+++ cwltool/main.py
@@ -33,6 +33,7 @@ from .process import (Process, cleanIntermediate, normalizeFilesDirs,
 from .resolver import ga4gh_tool_registries, tool_resolver
 from .stdfsaccess import StdFsAccess
 from .update import ALLUPDATES, UPDATES
+from ruamel.yaml.comments import Comment, CommentedSeq, CommentedMap
 
 _logger = logging.getLogger("cwltool")
 
@@ -235,13 +236,6 @@ def single_job_executor(t,  # type: Process
     if jobReqs:
         for req in jobReqs:
             t.requirements.append(req)
-
-    if kwargs.get("default_container"):
-        t.hints.insert(0, {
-            "class": "DockerRequirement",
-            "dockerPull": kwargs["default_container"],
-            "requirement":True
-        })
 
     jobiter = t.job(job_order_object,
                     output_callback,
@@ -620,10 +614,17 @@ def main(argsl=None,  # type: List[str]
                 argsl = sys.argv[1:]
             args = arg_parser().parse_args(argsl)
 
-
         # If On windows platform, A default Docker Container is Used if not explicitely provided by user
         if os.name == 'nt' and not args.default_container:
             args.default_container = "ubuntu"
+
+        # Making default Container as lowest priority, that should be used if nothing overrides it
+        if args.default_container:
+            defContainer = CommentedMap()
+            defContainer.insert(1, "class", "DockerRequirement")
+            defContainer.insert(0, "dockerPull", args.default_container)
+            defContainer.insert(2, "requirement", True)
+            args.hints=[defContainer]
 
         # If caller provided custom arguments, it may be not every expected
         # option is set, so fill in no-op defaults to avoid crashing when
