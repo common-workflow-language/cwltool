7d40988ded31d5af52743ede7b569f8546eac60e
Michael R. Crusoe <michael.crusoe@gmail.com>
2017-07-17 06:03:17 -0700

    match matrix project layout

--- jenkins.bash
+++ jenkins.bash
@@ -13,6 +13,7 @@ venv() {
 	# shellcheck source=/dev/null
         source "$1"/bin/activate
 }
+git clean --force -d -x || /bin/true
 cloneorpull common-workflow-language https://github.com/common-workflow-language/common-workflow-language.git
 venv cwltool-venv
 docker pull node:slim
@@ -22,9 +23,9 @@ python setup.py install
 pip install "cwltest>=1.0.20160825151655"
 pushd common-workflow-language
 git clean --force -d -x || /bin/true
-./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=draft-3 && \
-	./run_test.sh --junit-xml=result.xml RUNNER=cwltool && \
-	./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=v1.1.0-dev1 EXTRA=--enable-dev
+if [ $version = *dev ] then
+	EXTRA="EXTRA=--enable-dev"
+./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=${version} ${EXTRA}
 CODE=$?
 popd
 if [ "$GIT_BRANCH" = "origin/master" ]
@@ -32,4 +33,4 @@ then
   ./build-cwl-docker.sh && docker push commonworkflowlanguage/cwltool_module && docker push commonworkflowlanguage/cwltool
 fi
 #docker rm -v $(docker ps -a -f status=exited | sed 's/  */ /g' | cut -d' ' -f1)
-return ${CODE}
+exit ${CODE}
