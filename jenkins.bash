8ede056a97a755b9a4d7d0030ba6dfc49201d44d
Michael R. Crusoe <michael.crusoe@gmail.com>
2017-07-17 05:43:01 -0700

    now for PRs too

--- jenkins.bash
+++ jenkins.bash
@@ -1,24 +1,35 @@
 #!/usr/bin/bash
 cloneorpull() {
-        if test -d $1 ; then
-                (cd $1 && git pull)
+        if test -d "$1" ; then
+                (cd "$1" && git pull)
         else
-                git clone $2
+                git clone "$2"
         fi
 }
 venv() {
-        if ! test -d $1 ; then
-                virtualenv $1
+        if ! test -d "$1" ; then
+                virtualenv "$1"
         fi
-        . $1/bin/activate
+	# shellcheck source=/dev/null
+        source "$1"/bin/activate
 }
 cloneorpull common-workflow-language https://github.com/common-workflow-language/common-workflow-language.git
 venv cwltool-venv
-(. cwltool-venv/bin/activate && PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/ pip install -U setuptools wheel pip)
-(. cwltool-venv/bin/activate && PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/ python setup.py install)
-(. cwltool-venv/bin/activate && PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/ pip install "cwltest>=1.0.20160825151655")
-# (. cwltool-venv/bin/activate && cd common-workflow-language && ./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=draft-2)
-(. cwltool-venv/bin/activate && cd common-workflow-language && ./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=draft-3)
-(. cwltool-venv/bin/activate && cd common-workflow-language && ./run_test.sh --junit-xml=result.xml RUNNER=cwltool)
-(. cwltool-venv/bin/activate && cd common-workflow-language && ./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=v1.1.0-dev1 EXTRA=--enable-dev)
-(. cwltool-venv/bin/activate && ./build-cwl-docker.sh && docker push commonworkflowlanguage/cwltool_module && docker push commonworkflowlanguage/cwltool)
+docker pull node:slim
+export PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/
+pip install -U setuptools wheel pip
+python setup.py install
+pip install "cwltest>=1.0.20160825151655"
+pushd common-workflow-language
+git clean --force -d -x || /bin/true
+./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=draft-3 && \
+	./run_test.sh --junit-xml=result.xml RUNNER=cwltool && \
+	./run_test.sh --junit-xml=result.xml RUNNER=cwltool DRAFT=v1.1.0-dev1 EXTRA=--enable-dev
+CODE=$?
+popd
+if [ "$GIT_BRANCH" = "origin/master" ]
+then
+  ./build-cwl-docker.sh && docker push commonworkflowlanguage/cwltool_module && docker push commonworkflowlanguage/cwltool
+fi
+#docker rm -v $(docker ps -a -f status=exited | sed 's/  */ /g' | cut -d' ' -f1)
+return ${CODE}
