00aa5d1804f2532f2caf6a5c00fd046cf3c3dcb6
Stian Soiland-Reyes <stain@apache.org>
2017-07-20 22:41:13 +0100

    optional close

--- cwltool/provenance.py
+++ cwltool/provenance.py
@@ -8,35 +8,52 @@ import logging
 
 _logger = logging.getLogger("cwltool")
 
+# RO folders
 METADATA = "metadata"
 DATA = "data"
 WORKFLOW = "workflow"
-MAIN = os.path.join(WORKFLOW, "main")
 SNAPSHOT = "snapshot"
+# sub-folders
+MAIN = os.path.join(WORKFLOW, "main")
 PROVENANCE = os.path.join(METADATA, "provenance")
 
 class RO():
     def __init__(self, tmpPrefix="tmp"):
         self.folder = tempfile.mkdtemp(prefix=tmpPrefix)
+        self.initialize()
+        _logger.info("Temporary research object: %s", self.folder)
+
+    def _initialize(self):
         for f in (METADATA, DATA, WORKFLOW, SNAPSHOT, PROVENANCE):
             os.makedirs(os.path.join(self.folder, f))
-        _logger.info("Temporary research object: %s", self.folder)
+
+    def _finalize(self):
+        # TODO: Write manifest and bagit metadata
+        pass
 
     def packed_workflow(self, packed):
         path = os.path.join(self.folder, WORKFLOW, "packed.cwl")
         f = open(path, "w")
-        f.write(packed.encode("UTF8"))
+        # YAML is always UTF8
+        f.write(packed.encode("UTF-8"))
         f.close()
 
-    def close(self, folder):
+    def close(self, folder=None):
         """Close the Research Object after saving to specified folder.
+
+        If the argument 'folder' is None (the default value), the 
+        research object will be removed without saving.
 
         This function can only be called once, after which this object
         can no longer be used.
         """
-        # TODO: Write manifest and bagit metadata
-        # TODO: Add to archive (.zip or .tar)
-        shutil.move(self.folder, folder)
+        if folder is None:
+            shutil.rmtree(self.folder)
+        else:
+            self._finalize()
+            # TODO: Add to archive (.zip or .tar)
+            shutil.move(self.folder, folder)
+
         # Forget about our tempfolder which no longer exists
         self.folder = None
 
