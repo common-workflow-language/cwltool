864e6f1aa544ced28b16bce8fbf26809650393d9
Manvendra Singh <manvendra0310@gmail.com>
2017-07-30 00:02:04 +0530

    jenkins.bash: Add support for Python 3 conformance testing

--- jenkins.bash
+++ jenkins.bash
@@ -14,29 +14,37 @@ cloneorpull() {
 }
 venv() {
         if ! test -d "$1" ; then
-                virtualenv "$1"
+                virtualenv -p python${PYTHON_VERSION} "$1"
         fi
 	# shellcheck source=/dev/null
         source "$1"/bin/activate
 }
-git clean --force -d -x || /bin/true
-cloneorpull common-workflow-language https://github.com/common-workflow-language/common-workflow-language.git
-venv cwltool-venv
-docker pull node:slim
-export PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/
-pip install -U setuptools wheel pip
-python setup.py install
-pip install "cwltest>=1.0.20160825151655"
-pushd common-workflow-language
-git clean --force -d -x || /bin/true
-# shellcheck disable=SC2154
-if [[ "$version" = *dev* ]]
-then
-	EXTRA="EXTRA=--enable-dev"
-fi
-./run_test.sh --junit-xml=result.xml RUNNER=cwltool -j4 DRAFT="${version}" ${EXTRA}
-CODE=$?
-popd
+
+# Test for Python 2.7 and Python 3
+for PYTHON_VERSION in 2 3
+do
+	git clean --force -d -x || /bin/true
+	cloneorpull common-workflow-language https://github.com/common-workflow-language/common-workflow-language.git
+	docker pull node:slim
+	venv cwltool-venv
+	export PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/
+	pip install -U setuptools wheel pip
+	pip install .
+	pip install "cwltest>=1.0.20160825151655"
+	pushd common-workflow-language
+	git clean --force -d -x || /bin/true
+	# shellcheck disable=SC2154
+	if [[ "$version" = *dev* ]]
+	then
+		EXTRA="EXTRA=--enable-dev"
+	fi
+	./run_test.sh --junit-xml=result.xml RUNNER=cwltool -j4 DRAFT=${version}
+	deactivate
+	CODE=$?
+	popd
+done
+
+# build new docker container
 if [ "$GIT_BRANCH" = "origin/master" ] && [[ "$version" = "v1.0" ]]
 then
   ./build-cwl-docker.sh && docker push commonworkflowlanguage/cwltool_module && docker push commonworkflowlanguage/cwltool
