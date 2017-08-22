d59cdbdc34c329c93abeeb29ece705f08ad0f316
Peter Amstutz <peter.amstutz@curoverse.com>
2017-08-22 15:07:44 -0400

    Add type annotation to ensure_writable

--- cwltool/pathmapper.py
+++ cwltool/pathmapper.py
@@ -169,6 +169,7 @@ def downloadHttpFile(httpurl):
     return f.name
 
 def ensure_writable(path):
+    # type: (Text) -> None
     if os.path.isdir(path):
         for root, dirs, files in os.walk(path):
             for name in files:
