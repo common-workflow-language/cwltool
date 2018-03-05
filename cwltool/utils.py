4f126dbc5d5cc2dad95e7a186fda63d1c8299f1e
ThomasHickman <Thomas.Hickman42@gmail.com>
2018-03-05 21:39:48 +0000

    Add folder checking and exception to get_data

--- cwltool/utils.py
+++ cwltool/utils.py
@@ -186,6 +186,10 @@ def get_data(filename):
             Requirement.parse("cwltool"), filename)
     except ResolutionError:
         pass
-    if not filepath or not os.path.isfile(filepath):
+    if not filepath or not (os.path.isfile(filepath) or os.path.isdir(filepath)):
         filepath = os.path.join(os.path.dirname(__file__), os.pardir, filename)
+
+    if os.path.isfile(filepath) or os.path.isdir(filepath):
+        raise Exception("Resource %s not found." % filepath)
+
     return filepath
