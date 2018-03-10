4062fb98a0486746a658b3fca0a59e0c9f965e36
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2018-03-10 15:10:56 +0100

    remove unneeded import

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 import unittest
 
 from cwltool.secrets import SecretStore
-from .util import get_data
 
 class TestSecrets(unittest.TestCase):
     def test_secrets(self):
