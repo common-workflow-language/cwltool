583dc99bbfefa3bc767f4eedb3282b82a3917d1b
ThomasHickman <Thomas.Hickman42@gmail.com>
2018-03-10 16:24:24 +0000

    globals -> js_globals

--- cwltool/validate_js.py
+++ cwltool/validate_js.py
@@ -167,11 +167,11 @@ def validate_js_expressions(tool, schema, jshint_options=None):
     else:
         return
 
-    globals = copy.deepcopy(default_globals)
+    js_globals = copy.deepcopy(default_globals)
 
     for i, expression_lib_line in enumerate(expression_lib):
-        expression_lib_line_errors, expression_lib_line_globals = jshint_js(expression_lib_line, globals, jshint_options)
-        globals.extend(expression_lib_line_globals)
+        expression_lib_line_errors, expression_lib_line_globals = jshint_js(expression_lib_line, js_globals, jshint_options)
+        js_globals.extend(expression_lib_line_globals)
         print_js_hint_messages(expression_lib_line_errors, SourceLine(expression_lib, i))
 
     expressions = get_expressions(tool, schema)
@@ -184,7 +184,7 @@ def validate_js_expressions(tool, schema, jshint_options=None):
             if unscanned_str[scan_slice[0]] == '$':
                 code_fragment = unscanned_str[scan_slice[0] + 1:scan_slice[1]]
                 code_fragment_js = code_fragment_to_js(code_fragment, "")
-                expression_errors, _ = jshint_js(code_fragment_js, globals, jshint_options)
+                expression_errors, _ = jshint_js(code_fragment_js, js_globals, jshint_options)
                 print_js_hint_messages(expression_errors, source_line)
 
             unscanned_str = unscanned_str[scan_slice[1]:]
