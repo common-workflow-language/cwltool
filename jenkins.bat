408232c3da0aa7b35a907070faf65896ee6dc381
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-11 04:51:11 -0700

    hardcode machine name

--- jenkins.bat
+++ jenkins.bat
@@ -1,7 +1,6 @@
-set BOOT2DOCKER_VM=default
 set PATH=%PATH%;"C:\\Program Files\\Docker Toolbox\\"
-docker-machine start %BOOT2DOCKER_VM%
+docker-machine start default
 REM Set the environment variables to use docker-machine and docker commands
-@FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd %BOOT2DOCKER_VM%') DO @%
+@FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd default') DO @%i
 
 python setup.py test --addopts --junit-xml=tests.xml
