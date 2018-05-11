3aa1c61252e1f2cde15a6c35e7465a5f7a575b4a
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-11 11:56:26 -0700

    be more explicit

--- .jenkins/conformance.bat
+++ .jenkins/conformance.bat
@@ -1,4 +1,4 @@
 CALL %~dp0\setup.bat
 
 pip install cwltest %WORKSPACE%
-cwltest --test=common-workflow-language\v1.0\conformance_test_v1.0.yaml --classname win7py36 --tool cwltool --junit-xml conformance.xml -j 2 --basedir common-workflow-language\v1.0
+cwltest --test=%WORKSPACE%\common-workflow-language\v1.0\conformance_test_v1.0.yaml --classname win7py36 --tool cwltool --junit-xml %WORKSPACE%\conformance.xml -j 2 --basedir %WORKSPACE%\common-workflow-language\v1.0\
