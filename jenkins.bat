2e21932b4bbb4672b5c8d073dc025071a2243f88
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-11 06:01:06 -0700

    measure code coverage on windows

--- jenkins.bat
+++ jenkins.bat
@@ -3,4 +3,6 @@ docker-machine start default
 REM Set the environment variables to use docker-machine and docker commands
 FOR /f "tokens=*" %%i IN ('docker-machine env --shell cmd default') DO %%i
 
-python setup.py test --addopts --junit-xml=tests.xml
+python setup.py test --addopts "--junit-xml=tests.xml --cov-report xml --cov cwltool"
+pip install codecov
+codecov
