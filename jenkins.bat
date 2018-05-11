854234df05c28445a29127825e04567c07afb684
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-11 04:57:45 -0700

    double escape %

--- jenkins.bat
+++ jenkins.bat
@@ -1,6 +1,6 @@
 set PATH=%PATH%;"C:\\Program Files\\Docker Toolbox\\"
 docker-machine start default
 REM Set the environment variables to use docker-machine and docker commands
-@FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd default') DO @%i
+FOR /f "tokens=*" %%i IN ('docker-machine env --shell cmd default') DO %%i
 
 python setup.py test --addopts --junit-xml=tests.xml
