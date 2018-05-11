6e5e53a4212d81f5e42ecc98f3e14b88edf59da5
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-11 09:37:59 -0700

    and this?

--- .jenkins/test.bat
+++ .jenkins/test.bat
@@ -1,6 +1,6 @@
-CALL .jenkins\setup.bat
-echo %%cd%%
-echo %%~dp0
+CALL %WORKSPACE%\setup.bat
+echo %cd%
+echo %~dp0
 pip install pytest pytest-cov
 python setup.py test --addopts "--junit-xml=tests.xml --cov-report xml --cov cwltool"
 pip install codecov
