fa6bfa4fb3352692334548478f43d02a45b1d6e2
Farah Zaib Khan <farahk@student.unimelb.edu.au>
2018-05-23 23:39:09 +1000

    remove duplicate function call

--- cwltool/command_line_tool.py
+++ cwltool/command_line_tool.py
@@ -347,7 +347,6 @@ class CommandLineTool(Process):
                     partial(rm_pending_output_callback, output_callbacks,
                             jobcachepending))
 
-        builder = self._init_job(job_order, provObj, **kwargs)
         if kwargs["research_obj"]:
             builder = self._init_job(job_order, provObj, **kwargs)
         else:
