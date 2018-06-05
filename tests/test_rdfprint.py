138d36d1bf2d6055b324173b2df040d4d325bf23
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-06-05 11:08:22 +0200

    add test for UTF with --print-rdf

--- tests/test_rdfprint.py
+++ tests/test_rdfprint.py
@@ -1,12 +1,24 @@
 from __future__ import absolute_import
 import unittest
+import os
+import sys
 
-from six import StringIO
 from cwltool.main import main
+from cwltool.utils import subprocess
 
 from .util import get_data
 
-class RDF_Print(unittest.TestCase):
+class RDFPrint(unittest.TestCase):
 
     def test_rdf_print(self):
         self.assertEquals(main(['--print-rdf', get_data('tests/wf/hello_single_tool.cwl')]), 0)
+
+    def test_rdf_print_unicode(self):
+        """Force ASCII encoding but load UTF file with --print-rdf."""
+        lc_all=os.environ.get("LC_ALL", None)
+        os.environ["LC_ALL"]="C"
+        self.assertEquals(subprocess.check_call(
+            [sys.executable, "-m", "cwltool", '--print-rdf',
+             get_data('tests/utf_doc_example')]), 0)
+        if lc_all:
+                os.environ["LC_ALL"]=lc_all
