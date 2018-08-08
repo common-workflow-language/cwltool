7ccc3f7fbbf04550fac65c7838f1fddeb22c15b6
Fotis Georgatos <kefalonia@gmail.com>
2018-08-08 16:43:59 +0200

    typo fix

--- cwltool.py
+++ cwltool.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
-"""Convienance entry point for cwltool.
+"""Convenience entry point for cwltool.
 
 This can be used instead of the recommended method of `./setup.py install`
 or `./setup.py develop` and then using the generated `cwltool` executable.
