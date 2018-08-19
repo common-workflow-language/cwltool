4719b8be411dcd93978196fecfb135c3979b222f
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-08-19 05:17:00 -0700

    enanhce debug output

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -18,5 +18,7 @@ class TestBetaDependenciesResolver(TestCmdLine):
         wflow = get_data("tests/seqtk_seq.cwl")
         job = get_data("tests/seqtk_seq_job.json")
         error_code, stdout, stderr = self.get_main_output(
-            ["--beta-conda-dependencies", wflow, job])
+            ["--beta-conda-dependencies", "--debug", wflow, job])
+        print(stderr)
+        print(stdout)
         assert error_code is 0
