96b18cbedc5c4415cd9990c105eab3fda6184f12
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-08-19 14:22:03 +0300

    deps, everywhere

--- release-test.sh
+++ release-test.sh
@@ -29,6 +29,7 @@ then
 		&& pip install --force-reinstall -U pip==${pipver} \
 	        && pip install setuptools==${setuptoolsver} wheel
 	make install-dep
+	pip install 'galaxy-lib>=17.09.3'
 	make test
 	pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
 	mkdir testenv1/not-${module}
@@ -52,7 +53,7 @@ source bin/activate
 rm lib/python-wheels/setuptools* \
 	&& pip install --force-reinstall -U pip==${pipver} \
         && pip install setuptools==${setuptoolsver} wheel
-pip install -e "git+${repo}@${HEAD}#egg=${package}"
+pip install -e "git+${repo}@${HEAD}#egg=${package}[deps]"
 cd src/${package}
 make install-dep
 make dist
@@ -72,7 +73,7 @@ source bin/activate
 rm lib/python-wheels/setuptools* \
 	&& pip install --force-reinstall -U pip==${pipver} \
         && pip install setuptools==${setuptoolsver} wheel
-pip install ${package}*tar.gz
+pip install ${package}*tar.gz[deps]
 pip install "-r${DIR}/test-requirements.txt"
 mkdir out
 tar --extract --directory=out -z -f ${package}*.tar.gz
