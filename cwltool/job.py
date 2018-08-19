b9537158413bddbf3f541007ba008bbdbe79ac8d
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-08-19 05:55:31 -0700

    Don't set close_fds on windows

--- cwltool/job.py
+++ cwltool/job.py
@@ -79,9 +79,10 @@ with open(sys.argv[1], "r") as f:
         stderr = open(stderr_path, "wb")
     else:
         stderr = sys.stderr
+    close_fds = not os.name == 'nt'
     sp = subprocess.Popen(commands,
                           shell=False,
-                          close_fds=True,
+                          close_fds=close_fds,
                           stdin=stdin,
                           stdout=stdout,
                           stderr=stderr,
