07a3c4e5440dd1de061ebd6333d91a95bb6f8c40
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-09-06 11:31:28 +0100

    maintenance: remove unittest from test_docker_warning

--- tests/test_docker_warning.py
+++ tests/test_docker_warning.py
@@ -1,30 +1,26 @@
-from __future__ import absolute_import
-
-import unittest
-
-from mock import mock
-
-from cwltool.command_line_tool import DEFAULT_CONTAINER_MSG, CommandLineTool
-from cwltool.context import RuntimeContext
+from cwltool import command_line_tool
 from cwltool.utils import windows_default_container_id
+from cwltool.context import RuntimeContext
 
 
-class TestDefaultDockerWarning(unittest.TestCase):
+# Test to check warning when default docker Container is used on Windows
+def test_default_docker_warning(mocker):
+    mocker.patch("cwltool.command_line_tool.onWindows", return_value=True)
+    mocker.patch("cwltool.command_line_tool._logger")
 
-    # Test to check warning when default docker Container is used on Windows
-    @mock.patch("cwltool.command_line_tool.onWindows",return_value = True)
-    @mock.patch("cwltool.command_line_tool._logger")
-    def test_default_docker_warning(self,mock_logger,mock_windows):
+    class TestCommandLineTool(command_line_tool.CommandLineTool):
+        def __init__(self, **kwargs):
+            self.requirements = []
+            self.hints = []
 
-        class TestCommandLineTool(CommandLineTool):
-            def __init__(self, **kwargs):
-                self.requirements=[]
-                self.hints=[]
+        def find_default_container(self, args, builder):
+            return windows_default_container_id
 
-            def find_default_container(self, args, builder):
-                return windows_default_container_id
+    tool = TestCommandLineTool()
+    tool.make_job_runner(RuntimeContext({
+        "find_default_container": lambda x: "frolvlad/alpine-bash"}))
 
-        TestObject = TestCommandLineTool()
-        TestObject.make_job_runner(RuntimeContext({
-            "find_default_container": lambda x: "frolvlad/alpine-bash"}))
-        mock_logger.warning.assert_called_with(DEFAULT_CONTAINER_MSG, windows_default_container_id, windows_default_container_id)
+    command_line_tool._logger.warning.assert_called_with(
+        command_line_tool.DEFAULT_CONTAINER_MSG,
+        windows_default_container_id,
+        windows_default_container_id)
