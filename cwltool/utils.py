a61ae1f527c07e4cce8bf0f7598a278b3e425cdb
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2018-09-10 14:42:29 +0300

    one last type change

--- cwltool/utils.py
+++ cwltool/utils.py
@@ -217,4 +217,4 @@ def random_outdir():  # type: () -> Text
     # compute this once and store it as a function attribute - each subsequent call will return the same value
     if not hasattr(random_outdir, 'outdir'):
         random_outdir.outdir = '/' + ''.join([random.choice(string.ascii_letters) for _ in range(6)])  # type: ignore
-    return random_outdir.outdir
+    return random_outdir.outdir  # type: ignore
