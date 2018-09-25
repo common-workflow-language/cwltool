66d8c23355fec7e7b2edd75ac32c5cc2ced7f615
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2018-09-25 15:06:53 +0300

    cwlprov: always add UTC timestamps (#920)

--- cwltool/main.py
+++ cwltool/main.py
@@ -11,6 +11,7 @@ import logging
 import os
 import signal
 import sys
+import time
 from codecs import StreamWriter, getwriter  # pylint: disable=unused-import
 from typing import (IO, Any, Callable, Dict, Iterable, List, Mapping,
                     MutableMapping, MutableSequence, Optional, TextIO, Tuple,
@@ -551,8 +552,19 @@ def main(argsl=None,                   # type: List[str]
                 runtimeContext.research_obj.folder, "log")
             prov_log_handler = logging.FileHandler(
                 str(prov_log_handler_filename))
-            if formatter:
-                prov_log_handler.setFormatter(formatter)
+            class ProvLogFormatter(logging.Formatter):
+                """Enforce ISO8601 with both T and Z."""
+                def __init__(self):  # type: () -> None
+                    super(ProvLogFormatter, self).__init__(
+                        "[%(asctime)sZ] %(message)s")
+
+                def formatTime(self, record, datefmt=None):
+                    # type: (logging.LogRecord, str) -> str
+                    record_time = time.gmtime(record.created)
+                    formatted_time = time.strftime("%Y-%m-%dT%H:%M:%S", record_time)
+                    with_msecs = "%s,%03d" % (formatted_time, record.msecs)
+                    return with_msecs
+            prov_log_handler.setFormatter(ProvLogFormatter())
             _logger.addHandler(prov_log_handler)
 
         if loadingContext is None:
