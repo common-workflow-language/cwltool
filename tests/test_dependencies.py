cdee024077ed4e0730a60c3503c6f78790ef2e5a
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-10-12 08:50:15 -0700

    share depends_dir

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,7 +1,8 @@
+from typing_extensions import Text
+
 import pytest
 
 from cwltool.utils import onWindows
-
 from .util import get_data, get_main_output, needs_docker, temp_dir
 
 
@@ -10,54 +11,55 @@ try:
 except ImportError:
     deps = None
 
+
+@pytest.fixture(scope="module")
+def depends_dir(tmpdir_factory):
+    ddir = tmpdir_factory.mktemp("cwltool_deps")
+    yield Text(ddir)
+    ddir.remove()
+
 @needs_docker
 @pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
-def test_biocontainers():
+def test_biocontainers(depends_dir):
     wflow = get_data("tests/seqtk_seq.cwl")
     job = get_data("tests/seqtk_seq_job.json")
-    with temp_dir() as depends_dir:
-        error_code, _, _ = get_main_output(
-            ["--beta-use-biocontainers", "--beta-dependencies-directory",
-             depends_dir, wflow, job])
+    error_code, _, _ = get_main_output(
+        ["--beta-use-biocontainers", "--beta-dependencies-directory",
+         depends_dir, wflow, job])
 
     assert error_code == 0
 
 @pytest.mark.skipif(onWindows(), reason="bioconda currently not working on MS Windows")
 @pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
-def test_bioconda():
+def test_bioconda(depends_dir):
     wflow = get_data("tests/seqtk_seq.cwl")
     job = get_data("tests/seqtk_seq_job.json")
-    with temp_dir() as depends_dir:
-        error_code, _, stderr = get_main_output(
-            ["--beta-conda-dependencies", "--beta-dependencies-directory",
-             depends_dir, "--debug", wflow, job])
+    error_code, _, stderr = get_main_output(
+        ["--beta-conda-dependencies", "--beta-dependencies-directory",
+         depends_dir, "--debug", wflow, job])
 
     assert error_code == 0, stderr
 
 @pytest.mark.skipif(onWindows(), reason="bioconda currently not working on MS Windows")
 @pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
-def test_bioconda_wrong_name():
+def test_bioconda_wrong_name(depends_dir):
     wflow = get_data("tests/seqtk_seq_wrong_name.cwl")
     job = get_data("tests/seqtk_seq_job.json")
-    with temp_dir() as depends_dir:
-        error_code, _, stderr = get_main_output(
-            ["--beta-conda-dependencies", "--beta-dependencies-directory",
-             depends_dir, "--debug", wflow, job])
+    error_code, _, stderr = get_main_output(
+        ["--beta-conda-dependencies", "--beta-dependencies-directory",
+         depends_dir, "--debug", wflow, job])
 
     assert error_code == 0, stderr
 
 @pytest.mark.skipif(onWindows(), reason="custom dependency resolver is Unix only for now.")
 @pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
-def test_custom_config():
+def test_custom_config(depends_dir):
     config = get_data("tests/test_deps_env_resolvers_conf_rewrite.yml")
     wflow = get_data("tests/random_lines_mapping.cwl")
     job = get_data("tests/random_lines_job.json")
-    with temp_dir() as depends_dir:
-        error_code, _, stderr = get_main_output(
-            ["--beta-dependency-resolvers-configuration", config,
-                "--beta-dependencies-directory", depends_dir, "--debug", wflow,
-             job])
+    error_code, _, stderr = get_main_output(
+        ["--beta-dependency-resolvers-configuration", config,
+         "--beta-dependencies-directory", depends_dir, "--debug", wflow,
+         job])
 
     assert error_code == 0, stderr
-
-
