de5278456b2f63f67280acb220631dad3e807078
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-10-25 05:50:35 -0700

    match codecov style to appveyor/travisCI/conformance test

--- jenkins.bat
+++ jenkins.bat
@@ -2,7 +2,12 @@ set PATH=%PATH%;"C:\\Program Files\\Docker Toolbox\\"
 docker-machine start default
 REM Set the environment variables to use docker-machine and docker commands
 FOR /f "tokens=*" %%i IN ('docker-machine env --shell cmd default') DO %%i
+FOR /f "tokens=*" %%i IN ('docker-machine env --shell cmd default') DO %%i
 docker version
-python setup.py test --addopts "--junit-xml=tests.xml --cov-report xml --cov cwltool"
+pip install -U codecov pytest-xdist pytest-cov galaxy-lib -rtest-requirements.txt
+python -m coverage run --parallel-mode -m pytest --strict -p no:cacheprovider --junit-xml=tests.xml
+python -m coverage combine
+python -m coverage report
+python -m coverage xml
 pip install codecov
-codecov
+python.exe -m codecov --file coverage.xml
