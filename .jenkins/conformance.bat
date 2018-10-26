cc2d31f1fee62c654195467b4a79415f6618ed71
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-10-26 06:18:55 -0700

    move into correct directory

--- .jenkins/conformance.bat
+++ .jenkins/conformance.bat
@@ -3,4 +3,6 @@ CALL %~dp0\setup.bat
 pip install cwltest %WORKSPACE%
 rd /s /q %WORKSPACE%\cwl
 git clone https://github.com/common-workflow-language/common-workflow-language.git %WORKSPACE%\cwl
+pushd %WORKSPACE%\cwl\v1.0
 cwltest --test=%WORKSPACE%\cwl\v1.0\conformance_test_v1.0.yaml --classname win7py36 --tool cwltool --junit-xml %WORKSPACE%\conformance.xml -j 2 --basedir %WORKSPACE%\cwl\v1.0\
+popd
