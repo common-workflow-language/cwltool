6ea104137d7fac591f0a82514e9f9a3baf7feb1e
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-11-18 12:32:32 -0800

    make iwdr tests more cross-platform
    tempfile.NamedTemporaryFile() can't be re-opened on MS Windows

--- tests/test_iwdr.py
+++ tests/test_iwdr.py
@@ -1,3 +1,4 @@
+import os.path
 import tempfile
 from cwltool.main import main
 from cwltool import load_tool
@@ -20,26 +21,31 @@ def test_iwdr_permutations():
         with temp_dir() as sixth:
             with temp_dir() as seventh:
                 with temp_dir() as eighth:
-                    with tempfile.NamedTemporaryFile(mode='w+t') as first:
-                        first.write("1")
-                        first.flush()
-                        with tempfile.NamedTemporaryFile(mode='w+t') as second:
-                            second.write("2")
-                            second.flush()
-                            with tempfile.NamedTemporaryFile(mode='w+t') as third:
-                                third.write("3")
-                                third.flush()
-                                with tempfile.NamedTemporaryFile(mode='w+t') as fourth:
-                                    fourth.write("4")
-                                    fourth.flush()
+                    with temp_dir() as firstdir:
+                        first_name = os.path.join(firstdir, "first")
+                        with open(first_name, 'w+t') as first:
+                            first.write("1")
+                        with temp_dir() as seconddir:
+                            second_name = os.path.join(seconddir, "second")
+                            with open(second_name, 'w+t') as second:
+                                second.write("2")
+                            with  temp_dir() as thirddir:
+                                third_name = os.path.join(thirddir, "third")
+                                with open(third_name, 'w+t') as third:
+                                    third.write("3")
+                                with temp_dir() as fourthdir:
+                                    fourth_name = os.path.join(fourthdir,
+                                                               "fourth")
+                                    with open(fourth_name, 'w+t') as fourth:
+                                        fourth.write("4")
                                     with temp_dir() as outdir:
                                         assert(main(
                                             ['--outdir', outdir,
                                              get_data("tests/wf/iwdr_permutations.cwl"),
-                                             '--first', first.name,
-                                             '--second', second.name,
-                                             '--third', third.name,
-                                             '--fourth', fourth.name,
+                                             '--first', first_name,
+                                             '--second', second_name,
+                                             '--third', third_name,
+                                             '--fourth', fourth_name,
                                              '--fifth', fifth,
                                              '--sixth', sixth,
                                              '--seventh', seventh,
@@ -52,10 +58,23 @@ def test_iwdr_permutations_inplace():
         with temp_dir() as sixth:
             with temp_dir() as seventh:
                 with temp_dir() as eighth:
-                    with tempfile.NamedTemporaryFile() as first:
-                        with tempfile.NamedTemporaryFile() as second:
-                            with tempfile.NamedTemporaryFile() as third:
-                                with tempfile.NamedTemporaryFile() as fourth:
+                    with temp_dir() as firstdir:
+                        first_name = os.path.join(firstdir, "first")
+                        with open(first_name, 'w+t') as first:
+                            first.write("1")
+                        with temp_dir() as seconddir:
+                            second_name = os.path.join(seconddir, "second")
+                            with open(second_name, 'w+t') as second:
+                                second.write("2")
+                            with  temp_dir() as thirddir:
+                                third_name = os.path.join(thirddir, "third")
+                                with open(third_name, 'w+t') as third:
+                                    third.write("3")
+                                with temp_dir() as fourthdir:
+                                    fourth_name = os.path.join(fourthdir,
+                                                               "fourth")
+                                    with open(fourth_name, 'w+t') as fourth:
+                                        fourth.write("4")
                                     with temp_dir() as outdir:
                                         assert(main(
                                             ['--outdir', outdir,
@@ -63,10 +82,10 @@ def test_iwdr_permutations_inplace():
                                              '--overrides',
                                              get_data("tests/wf/iwdr_permutations_inplace.yml"),
                                              get_data("tests/wf/iwdr_permutations.cwl"),
-                                             '--first', first.name,
-                                             '--second', second.name,
-                                             '--third', third.name,
-                                             '--fourth', fourth.name,
+                                             '--first', first_name,
+                                             '--second', second_name,
+                                             '--third', third_name,
+                                             '--fourth', fourth_name,
                                              '--fifth', fifth,
                                              '--sixth', sixth,
                                              '--seventh', seventh,
