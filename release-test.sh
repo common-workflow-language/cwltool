cb48becf4ea0107571ac5ccd16351b04fd481f80
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-11-23 17:18:33 +0000

    maintenance: clean and clarify release-test

--- release-test.sh
+++ release-test.sh
@@ -9,25 +9,14 @@ package=cwltool
 module=cwltool
 slug=${TRAVIS_PULL_REQUEST_SLUG:=common-workflow-language/cwltool}
 repo=https://github.com/${slug}.git
-parallel=""
-if [[ "${OSTYPE}" == linux* ]]
-then
-	parallel=-n$(( $(nproc) / 2 ))
-fi
-if [[ "${OSTYPE}" == darwin* ]]
-then
-	parallel=-n$(( $(sysctl -n hw.physicalcpu) / 2 ))
-fi
-test_prefix=""
-run_tests() {
+run_tests_installed_package() {
 	local mod_loc
 	mod_loc=$(pip show ${package} |
 		grep ^Location | awk '{print $2}')/${module}
-	${test_prefix}bin/py.test "--ignore=${mod_loc}/schemas/" \
-		--pyarg -x ${module} ${parallel} --dist=loadfile
+	pytest "--ignore=${mod_loc}/schemas/" \
+		--pyargs -x ${module} -n auto --dist=loadfile
 }
 pipver=10.0.0 # required to install build deps
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 rm -Rf testenv? || /bin/true
 
@@ -36,64 +25,76 @@ export HEAD=${TRAVIS_PULL_REQUEST_SHA:-$(git rev-parse HEAD)}
 if [ "${RELEASE_SKIP}" != "head" ]
 then
 	virtualenv testenv1
+	virtualenv testenv0
 	# First we test the head
 	# shellcheck source=/dev/null
 	source testenv1/bin/activate
-	rm -Rf testenv1/local
-	rm testenv1/lib/python-wheels/setuptools* \
-		&& pip install --force-reinstall -U pip\>=${pipver}
-	make install-dep
+	rm -Rf testenv1/{local,lib/python-wheels/setuptools*}
+	pip install --force-reinstall -U pip\>=${pipver}
+	pip install .[test,deps]
 	make test
 	pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
 	mkdir testenv1/not-${module}
-	# if there is a subdir named '${module}' py.test will execute tests
-	# there instead of the installed module's tests
+	# Avoid having a subdir named '${module}' pytest may execute tests from that dir
+	# instead of the installed module's tests
 	pushd testenv1/not-${module}
 	# shellcheck disable=SC2086
-	test_prefix=../ run_tests; popd
+	run_tests_installed_package
+	deactivate; popd
 fi
 
 virtualenv testenv2
 virtualenv testenv3
-rm -Rf testenv[23]/local
+virtualenv testenv4
+rm -Rf testenv[234]/local
 
-# Secondly we test via pip
+# Install the package using git, run the tests in-place
+# Create a package to later run tests on those
 
-cd testenv2
 # shellcheck source=/dev/null
-source bin/activate
-rm lib/python-wheels/setuptools* \
-	&& pip install --force-reinstall -U pip\>=${pipver}
-pip install -e "git+${repo}@${HEAD}#egg=${package}"  #[deps]
-cd src/${package}
-make install-dep
-make dist
+source testenv2/bin/activate
+rm -Rf testenv2/{local,src,lib/python-wheels/setuptools*}
+pushd testenv2
+pip install --force-reinstall -U pip\>=${pipver}
+pip install -e "git+${repo}@${HEAD}#egg=${package}[test,deps]"
+pushd src/${package}
 make test
-cp dist/${package}*tar.gz ../../../testenv3/
-pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
-cd ../.. # no subdir named ${proj} here, safe for py.testing the installed module
-# shellcheck disable=SC2086
-run_tests
+make dist
+pushd dist
+package_tar=(${package}*.tar.gz)
+popd
+cp dist/${package_tar} ../../../testenv3/
+deactivate; popd; popd
 
 # Is the distribution in testenv2 complete enough to build another
 # functional distribution?
 
-cd ../testenv3/
 # shellcheck source=/dev/null
-source bin/activate
-rm lib/python-wheels/setuptools* \
-	&& pip install --force-reinstall -U pip\>=${pipver}
-package_tar=${package}*tar.gz
-pip install "${DIR}[test,deps]"
-pip install "${package_tar}"  # [deps]
+source testenv3/bin/activate
+rm -Rf testenv3/{local,lib/python-wheels/setuptools*}
+pushd testenv3/
+pip install --force-reinstall -U pip\>=${pipver}
+pip install "${package_tar}""[test,deps]"
 mkdir out
-tar --extract --directory=out -z -f ${package}*.tar.gz
-cd out/${package}*
+tar --extract --directory=out -z -f "${package_tar}"
+pushd out/${package}*
 make install-dep
-make dist
 make test
-pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
-mkdir ../not-${module}
-pushd ../not-${module}
+# Avoid having a subdir named '${module}' pytest may execute tests from that dir
+# instead of the installed module's tests
+popd; rm -Rf out/
 # shellcheck disable=SC2086
-test_prefix=../../ run_tests; popd
+run_tests_installed_package
+deactivate; popd
+
+# Install the package using git, run the tests from the package
+
+# shellcheck source=/dev/null
+source testenv4/bin/activate
+rm -Rf testenv4/{local,lib/python-wheels/setuptools*}
+pushd testenv4
+pip install "git+${repo}@${HEAD}#egg=${package}""[test,deps]"
+# no subdir named ${proj} here, safe for testing the installed module
+# shellcheck disable=SC2086
+run_tests_installed_package
+deactivate; popd;
