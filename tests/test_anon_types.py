01dd696860529db31d579ed9eeb3946adbcc33fe
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-11-30 05:36:41 -0800

    add another test

--- tests/test_anon_types.py
+++ tests/test_anon_types.py
@@ -4,7 +4,7 @@ from cwltool.command_line_tool import CommandLineTool
 from cwltool.context import LoadingContext
 
 
-snippet = {
+snippet = [{
     "cwlVersion": "v1.0",
     "class": "CommandLineTool",
     "inputs": [
@@ -46,7 +46,62 @@ snippet = {
     "baseCommand": "echo",
     "outputs": [],
     "id": "anon_enum_inside_array.cwl",
-}
+},{
+    "cwlVersion": "v1.0",
+    "class": "CommandLineTool",
+    "requirements": [
+        {
+            "types": [
+                {
+                    "name": "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params",
+                    "type": "record",
+                    "fields": [
+                        {
+                            "type": [
+                                "null",
+                                {
+                                    "type": "enum",
+                                    "symbols": [
+                                        "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/ncbi_build/GRCh37",
+                                        "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/ncbi_build/GRCh38",
+                                        "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/ncbi_build/GRCm38"
+                                    ]
+                                }
+                            ],
+                            "name": "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/ncbi_build"
+                        },
+                        {
+                            "type": [
+                                "null",
+                                {
+                                    "type": "enum",
+                                    "symbols": [
+                                        "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/species/homo_sapiens",
+                                        "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/species/mus_musculus"
+                                    ]
+                                }
+                            ],
+                            "name": "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params/species"
+                        }
+                    ]
+                }
+            ],
+            "class": "SchemaDefRequirement"
+        }
+    ],
+    "inputs": [
+        {
+            "type": "anon_enum_inside_array_inside_schemadef.cwl#vcf2maf_params",
+            "id": "anon_enum_inside_array_inside_schemadef.cwl#first"
+        }
+    ],
+    "baseCommand": "echo",
+    "outputs": [
+    ],
+    "id": "anon_enum_inside_array_inside_schemadef.cwl",
+}]
 
-def test_anon_types():
+
+@pytest.mark.parametrize('snippet', snippet)
+def test_anon_types(snippet):
     CommandLineTool(snippet, LoadingContext())
