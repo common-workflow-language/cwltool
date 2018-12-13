8587e06d914b6d64c4242d27865aea4440949a90
Miguel Boland <migueldboland@gmail.com>
2018-12-13 21:08:33 +0900

    Fixed pyenv version for osx

--- travis/os_install.sh
+++ travis/os_install.sh
@@ -14,11 +14,10 @@ elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
     # virtualenv doesn't work without pyenv knowledge. venv in Python 3.3
     # doesn't provide Pip by default. So, use `pyenv-virtualenv <https://github.com/yyuu/pyenv-virtualenv/blob/master/README.md>`_.
     brew install pyenv-virtualenv
-    pyenv install $PYTHON
+    pyenv install $PYENV_VERSION
     # I would expect something like ``pyenv init; pyenv local $PYTHON`` or
     # ``pyenv shell $PYTHON`` would work, but ``pyenv init`` doesn't seem to
     # modify the Bash environment. ??? So, I hand-set the variables instead.
-    export PYENV_VERSION=$PYTHON
     export PATH="/Users/travis/.pyenv/shims:${PATH}"
     pyenv-virtualenv venv
     source venv/bin/activate
