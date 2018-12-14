60ca9679d401d201f191f2d73aeca89bfd431775
Miguel Boland <migueldboland@gmail.com>
2018-12-14 14:04:16 +0900

    De-replicated mypy, setup TOXENV variable for osx

--- travis/os_install.sh
+++ travis/os_install.sh
@@ -6,6 +6,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update;
     sudo apt-get install -y singularity-container;
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    export TOXENV=$TRAVIS_PYTHON_VERSION
     brew update
     brew install openssl readline
     # See https://docs.travis-ci.com/user/osx-ci-environment/#A-note-on-upgrading-packages.
