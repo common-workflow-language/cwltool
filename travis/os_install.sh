4b73652352a39082b5bf739e042940e6b2adb591
Miguel Boland <migueldboland@gmail.com>
2018-12-14 23:38:33 +0900

    Fixed pyenv

--- travis/os_install.sh
+++ travis/os_install.sh
@@ -7,5 +7,10 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get install -y singularity-container;
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
     pyenv install $PYENV_VERSION;
+    eval "$(pyenv init -)";
     pyenv global $PYENV_VERSION;
+    pip install -U virtualenv;
+    VIRTUAL_ENV="$HOME/ve-pyenv-$PYENV_VERSION";
+    virtualenv -p "$(which python)" ${VIRTUALENV_EXTRA_ARGS:-} "$VIRTUAL_ENV";
+    source "$VIRTUAL_ENV/bin/activate";
 fi
