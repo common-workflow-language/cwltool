8ba1840a400f7054e09364596a6eea2b047c6214
Miguel Boland <migueldboland@gmail.com>
2018-12-14 17:52:44 +0900

    Switched to using system pyenv

--- travis/os_install.sh
+++ travis/os_install.sh
@@ -6,18 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update;
     sudo apt-get install -y singularity-container;
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    export TOXENV=$TRAVIS_PYTHON_VERSION
-    # virtualenv doesn't work without pyenv knowledge. venv in Python 3.3
-    # doesn't provide Pip by default. So, use `pyenv-virtualenv <https://github.com/yyuu/pyenv-virtualenv/blob/master/README.md>`_.
-    brew install pyenv-virtualenv
     pyenv install $PYENV_VERSION
-    # I would expect something like ``pyenv init; pyenv local $PYTHON`` or
-    # ``pyenv shell $PYTHON`` would work, but ``pyenv init`` doesn't seem to
-    # modify the Bash environment. ??? So, I hand-set the variables instead.
-    export PATH="/Users/travis/.pyenv/shims:${PATH}"
-    # A manual check that the correct version of Python is running.
     python --version
-
     brew cask install docker
     open /Applications/Docker.app
 fi
