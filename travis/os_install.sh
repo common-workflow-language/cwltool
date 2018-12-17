a492917d282169ec8e9dc60f4cd07df726b86cf9
Miguel Boland <migueldboland@gmail.com>
2018-12-17 15:44:17 +0000

    Fixed urls

--- travis/os_install.sh
+++ travis/os_install.sh
@@ -6,9 +6,9 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update;
     sudo apt-get install -y singularity-container;
 elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    if [[ $TRAVIS_PYTHON_NAME == '2.7' ]]; then
+    if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then
         PKG_URL='https://www.python.org/ftp/python/2.7.15/python-2.7.15-macosx10.9.pkg'
-    elif [[ $TRAVIS_PYTHON_NAME == '3.4' ]]; then
+    elif [[ $TRAVIS_PYTHON_VERSION == '3.4' ]]; then
         PKG_URL='https://www.python.org/ftp/python/3.4.4/python-3.4.4-macosx10.6.pkg'
     fi
     wget $PKG_URL -O install_python.pkg;
