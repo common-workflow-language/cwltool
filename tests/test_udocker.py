9b5776b70426ed0f785c21927fff3fc3a63b3c91
Peter Amstutz <pamstutz@veritasgenetics.com>
2018-12-17 10:51:24 -0500

    Add missing --enable-ext to udocker test.

--- tests/test_udocker.py
+++ tests/test_udocker.py
@@ -58,7 +58,7 @@ class TestUdocker:
     def test_udocker_should_display_memory_usage(self, tmpdir):
         cwd = tmpdir.chdir()
         error_code, stdout, stderr = get_main_output(
-            ["--default-container=debian",  "--user-space-docker-cmd=" + self.udocker_path,
+            ["--enable-ext", "--default-container=debian",  "--user-space-docker-cmd=" + self.udocker_path,
              get_data("tests/wf/timelimit.cwl"), "--sleep_time", "10"])
         cwd.chdir()
         tmpdir.remove(ignore_errors=True)
