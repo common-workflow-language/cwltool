ba4fae899a45bcec333d695d974c5611213fe85d
Michael R. Crusoe <michael.crusoe@gmail.com>
2019-09-06 11:58:46 +0900

    prov: cope with literals

--- cwltool/provenance.py
+++ cwltool/provenance.py
@@ -801,10 +801,13 @@ class ProvenanceProfile():
                 base += "/" + name
             for key, value in job_order.items():
                 prov_role = self.wf_ns["%s/%s" % (base, key)]
-                entity = self.declare_artefact(value)
-                self.document.used(
-                    process_run_id, entity, datetime.datetime.now(), None,
-                    {"prov:role": prov_role})
+                try:
+                    entity = self.declare_artefact(value)
+                    self.document.used(
+                        process_run_id, entity, datetime.datetime.now(), None,
+                        {"prov:role": prov_role})
+                except FileNotFoundError:
+                    pass
 
     def generate_output_prov(self,
                              final_output,    # type: Union[Dict[Text, Any], List[Dict[Text, Any]]]
@@ -1577,7 +1580,10 @@ class ResearchObject():
                 del structure["location"]
 
             for val in structure.values():
-                self._relativise_files(val)
+                try:
+                    self._relativise_files(val)
+                except FileNotFoundError:
+                    pass
             return
 
         if isinstance(structure, (str, Text)):
