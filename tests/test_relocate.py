4d450ed5dcb61601b07faff8b7cdb303148119bb
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-10-17 10:58:05 -0400

    Passes on both py2 and py3

--- tests/test_relocate.py
+++ tests/test_relocate.py
@@ -1,6 +1,10 @@
 import sys
 import json
-from io import BytesIO, StringIO
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
 
 from cwltool.main import main
 
@@ -13,13 +17,10 @@ def test_for_910():
 
 @needs_docker
 def test_for_conflict_file_names():
-    if sys.version_info[0] < 3:
-        stream = BytesIO()
-    else:
-        stream = StringIO()
+    stream = StringIO()
 
     with temp_dir() as tmp:
-        assert main(["--outdir", tmp, get_data('tests/wf/conflict.cwl')], stdout=stream) == 0
+        assert main(["--debug", "--outdir", tmp, get_data('tests/wf/conflict.cwl')], stdout=stream) == 0
 
     out = json.loads(stream.getvalue())
     assert out["b1"]["basename"] == out["b2"]["basename"]
