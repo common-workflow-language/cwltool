c22b943a9fc2166a5770db9c06b6529eb451dba4
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-11-25 22:47:35 -0500

    Annotate @windows_needs_docker

--- tests/test_dest.py
+++ tests/test_dest.py
@@ -16,47 +16,53 @@ import cwltool.process
 from .util import get_data, needs_docker, temp_dir, windows_needs_docker
 
 
-@needs_docker
+@windows_needs_docker
 def test_array_dest1():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/array-dest1.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/foo1"))
         assert os.path.isfile(os.path.join(out_dir, "bar/foo2"))
 
-@needs_docker
+@windows_needs_docker
 def test_array_dest2():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/array-dest2.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/foo1"))
         assert os.path.isfile(os.path.join(out_dir, "baz/foo2"))
 
+@windows_needs_docker
 def test_array_dest3():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/array-dest3.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar"))
         assert os.path.isfile(os.path.join(out_dir, "baz"))
 
+@windows_needs_docker
 def test_array_dest4():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/array-dest4.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/foo1"))
         assert os.path.isfile(os.path.join(out_dir, "bar/baz"))
 
+@windows_needs_docker
 def test_dest_expr():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/dest-expr.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/foo"))
 
+@windows_needs_docker
 def test_directory_dest1():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/directory-dest1.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/foo"))
 
+@windows_needs_docker
 def test_directory_dest2():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/directory-dest2.cwl')]) == 0
         assert os.path.isfile(os.path.join(out_dir, "bar/baz/foo"))
 
+@windows_needs_docker
 def test_file_rename():
     with temp_dir("out") as out_dir:
         assert main(["--enable-ext", "--outdir="+out_dir, get_data('tests/destination/file-rename.cwl')]) == 0
