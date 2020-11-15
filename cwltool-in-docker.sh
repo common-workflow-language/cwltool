144756ed75b67e2a7ca1ea715e2bc48b930fd337
Tomoya Tanjo <tanjo@nii.ac.jp>
2020-11-15 14:59:29 +0900

    Fix cwltool-in-docker.sh to accept DOCKER_HOST

--- cwltool-in-docker.sh
+++ cwltool-in-docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-if [ ! -S /var/run/docker.sock ]; then
+if [ \( ! -S /var/run/docker.sock \) -a \( -z "$DOCKER_HOST" \) ]; then
   >&2 echo 'ERROR: cwltool cannot work inside a container without access to docker'
   >&2 echo 'Launch the container with the option -v /var/run/docker.sock:/var/run/docker.sock'
+  >&2 echo 'or launch the container with the option to set $DOCKER_HOST'
   exit 1
 elif [ "$PWD" = '/error' ]; then
   >&2 echo 'ERROR: cwltool cannot work without access to the current path'
