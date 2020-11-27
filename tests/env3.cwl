3f6b4553e0e1ee445380dea5fb70ba908b356d60
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-11-27 20:35:55 +0000

    add a basic integration test for environment variables from modules

---
+++ tests/env3.cwl
@@ -0,0 +1,8 @@
+#!/usr/bin/env cwl-runner
+cwlVersion: v1.0
+class: CommandLineTool
+inputs: []
+baseCommand: env
+outputs:
+  env:
+    type: stdout
--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,11 +1,17 @@
+"""Tests of satisfying SoftwareRequirement via dependencies."""
+from io import StringIO
+import json
 import os
+from pathlib import Path
 from shutil import which
 from types import ModuleType
 from typing import Optional
 
 import pytest
 
-from .util import get_data, get_main_output, needs_docker
+from cwltool.main import main
+
+from .util import get_data, get_main_output, needs_docker, working_directory
 
 deps = None  # type: Optional[ModuleType]
 try:
@@ -37,11 +43,11 @@ def test_bioconda() -> None:
 
 @pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
 @pytest.mark.skipif(not which("modulecmd"), reason="modulecmd not installed")
-def test_modules() -> None:
+def test_modules(monkeypatch: pytest.MonkeyPatch) -> None:
     wflow = get_data("tests/random_lines.cwl")
     job = get_data("tests/random_lines_job.json")
-    os.environ["MODULEPATH"] = os.path.join(
-        os.getcwd(), "tests/test_deps_env/modulefiles"
+    monkeypatch.setenv(
+        "MODULEPATH", os.path.join(os.getcwd(), "tests/test_deps_env/modulefiles")
     )
     error_code, _, stderr = get_main_output(
         [
@@ -54,3 +60,37 @@ def test_modules() -> None:
     )
 
     assert error_code == 0, stderr
+
+
+@pytest.mark.skipif(not deps, reason="galaxy-lib is not installed")
+@pytest.mark.skipif(not which("modulecmd"), reason="modulecmd not installed")
+def test_modules_environment(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv(
+        "MODULEPATH", os.path.join(os.getcwd(), "tests/test_deps_env/modulefiles")
+    )
+
+    stdout = StringIO()
+    stderr = StringIO()
+    with working_directory(tmp_path):
+        rc = main(
+            argsl=[
+                "--beta-dependency-resolvers-configuration",
+                get_data("tests/test_deps_env_modules_resolvers_conf.yml"),
+                get_data("tests/env3.cwl"),
+                get_data("tests/env_with_software_req.yml"),
+            ],
+            stdout=stdout,
+            stderr=stderr,
+        )
+        assert rc == 0
+
+        output = json.loads(stdout.getvalue())
+        env_path = output["env"]["path"]
+        tool_env = {}
+        with open(env_path) as _:
+            for line in _:
+                key, val = line.split("=", 1)
+                tool_env[key] = val[:-1]
+    assert tool_env["TEST_VAR_MODULE"] == "environment variable ends in space "
+    tool_path = tool_env["PATH"].split(":")
+    assert get_data("tests/test_deps_env/random-lines/1.0/scripts") in tool_path
