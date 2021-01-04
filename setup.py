20a67ad6ec21eb182cc5c3cf2a19e30668cde933
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-01-04 14:11:09 +0100

    pin pytest version to Debian's development distribution version

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
     setup_requires=PYTEST_RUNNER,
     test_suite="tests",
     tests_require=[
-        "pytest < 7",
+        "pytest == 6.0.2",
         "mock >= 2.0.0",
         "pytest-mock >= 1.10.0",
         "arcp >= 0.2.0",
