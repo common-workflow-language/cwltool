ea6273d8969516621cadc709f7ed382257f963d1
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-03-09 10:22:11 +0100

    minimum setuptools & pip versions for Python3.9

--- release-test.sh
+++ release-test.sh
@@ -11,12 +11,11 @@ module=cwltool
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     # We are running as a GH Action
     repo=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}.git
-    HEAD=${GITHUB_SHA}
+    HEAD=${GITHUB_REF}
 else
     repo=https://github.com/common-workflow-language/cwltool.git
     HEAD=$(git rev-parse HEAD)
 fi
-
 test_prefix=""
 run_tests() {
 	local mod_loc
@@ -25,9 +24,8 @@ run_tests() {
 	"${test_prefix}"bin/py.test "--ignore=${mod_loc}/schemas/" \
 		--pyargs -x ${module} -n auto --dist=loadfile
 }
-pipver=7.0.2 # minimum required version of pip
-setuptoolsver=24.2.0 # required to generate correct metadata for
-                     # python_requires
+pipver=20.3b1 # minimum required version of pip for Python 3.9
+setuptoolsver=41.1.0 # required for Python 3.9
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 rm -Rf testenv? || /bin/true
@@ -35,12 +33,12 @@ rm -Rf testenv? || /bin/true
 
 if [ "${RELEASE_SKIP}" != "head" ]
 then
-	virtualenv testenv1 -p python3
+	python3 -m venv testenv_1
 	# First we test the head
 	# shellcheck source=/dev/null
 	source testenv1/bin/activate
 	rm -Rf testenv1/local
-	rm testenv1/lib/python-wheels/setuptools* \
+	rm -f testenv1/lib/python-wheels/setuptools* \
 		&& pip install --force-reinstall -U pip==${pipver} \
 	        && pip install setuptools==${setuptoolsver} wheel
 	make install-dep
@@ -55,9 +53,9 @@ then
 	test_prefix=../ run_tests; popd
 fi
 
-virtualenv testenv2 -p python3
-virtualenv testenv3 -p python3
-virtualenv testenv4 -p python3
+python3 -m venv testenv2
+python3 -m venv testenv3
+python3 -m venv testenv4
 rm -Rf testenv[234]/local
 
 # Secondly we test via pip
@@ -65,7 +63,7 @@ rm -Rf testenv[234]/local
 cd testenv2
 # shellcheck source=/dev/null
 source bin/activate
-rm lib/python-wheels/setuptools* \
+rm -f lib/python-wheels/setuptools* \
 	&& pip install --force-reinstall -U pip==${pipver} \
         && pip install setuptools==${setuptoolsver} wheel
 pip install -e "git+${repo}@${HEAD}#egg=${package}"  #[deps]
@@ -85,7 +83,7 @@ run_tests
 cd ../testenv3/
 # shellcheck source=/dev/null
 source bin/activate
-rm lib/python-wheels/setuptools* \
+rm -f lib/python-wheels/setuptools* \
 	&& pip install --force-reinstall -U pip==${pipver} \
         && pip install setuptools==${setuptoolsver} wheel
 package_tar=$(find . -name "${package}*tar.gz")
