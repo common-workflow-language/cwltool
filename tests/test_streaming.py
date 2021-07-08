f9b3968a0d1035d16f1af080b9d20fd4f1a26786
Mihai Popescu <mh.popescu12@gmail.com>
2021-07-08 16:43:11 +0300

    Fix test checks

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -1,10 +1,11 @@
+"""Test that files marked as 'streamable' when 'streaming_allowed' can be named pipes."""
 import os
 
 import pytest
 from pathlib import Path
 from typing import cast
 
-from ruamel.yaml import CommentedMap
+from ruamel.yaml.comments import CommentedMap
 from schema_salad.sourceline import cmap
 
 from cwltool.command_line_tool import CommandLineTool
@@ -12,7 +13,8 @@ from cwltool.context import LoadingContext, RuntimeContext
 from cwltool.errors import WorkflowException
 from cwltool.job import JobBase
 from cwltool.update import INTERNAL_VERSION
-from tests.util import get_data
+from cwltool.utils import CWLObjectType
+from .util import get_data
 
 toolpath_object = cast(
     CommentedMap,
@@ -44,7 +46,7 @@ loading_context = LoadingContext(
 
 
 def test_regular_file() -> None:
-    """Test that regular files do not raise any exception when they are checked in job._setup"""
+    """Test that regular files do not raise any exception when they are checked in job._setup."""
     clt = CommandLineTool(
         toolpath_object,
         loading_context,
@@ -66,9 +68,9 @@ def test_regular_file() -> None:
 
 streaming = [
     (True, True, False),
-     (True, False, True),
-     (False, True, True),
-     (False, False, True)
+    (True, False, True),
+    (False, True, True),
+    (False, False, True),
 ]
 
 
@@ -76,10 +78,7 @@ streaming = [
 def test_input_can_be_named_pipe(
     tmp_path: Path, streamable: bool, streaming_allowed: bool, raise_exception: bool
 ) -> None:
-    """
-    Test that input can be a named pipe only when
-    the file is marked as 'streamable' and 'streaming_allowed' is set
-    """
+    """Test that input can be a named pipe."""
     clt = CommandLineTool(
         toolpath_object,
         loading_context,
