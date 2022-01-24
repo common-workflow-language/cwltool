63cf0cdc23ee63a8de51249637db8982ef658405
Kevin Hannon <kannon1992@gmail.com>
2022-01-24 17:52:35 -0500

    feat: add a new test

--- tests/test_stdout_stderr_log_dir.py
+++ tests/test_stdout_stderr_log_dir.py
@@ -17,3 +17,17 @@ def test_log_dir_echo_output() -> None:
     )
     assert "completed success" in stderr, stderr
     assert json.loads(stdout)['out'].strip('\n') == 'hello'
+
+def test_log_dir_echo_no_output() -> None:
+    _, stdout, stderr = get_main_output(
+        [
+            "--log-dir",
+            "logs",
+            "tests/echo-stdout-log-dir.cwl",
+            "--inp", "hello"
+        ]
+    )
+    for dir in os.listdir('logs'):
+        for file in os.listdir(f'logs/{dir}'):
+            assert file == 'out.txt'
+
