d7a1441aef92a2c32dadc17d2c1d208da2ba8238
Kevin Hannon <kannon1992@gmail.com>
2022-01-25 10:03:31 -0500

    Update tests/test_stdout_stderr_log_dir.py
    Co-authored-by: Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>

--- tests/test_stdout_stderr_log_dir.py
+++ tests/test_stdout_stderr_log_dir.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 from cwltool.main import main
 
-from .util import get_main_output, needs_docker
+from .util import get_data, get_main_output, needs_docker
 
 
 def test_log_dir_echo_output() -> None:
     _, stdout, stderr = get_main_output(
-        ["--log-dir", "logs", "tests/echo.cwl", "--inp", "hello"]
+        ["--log-dir", "logs", get_data("tests/echo.cwl"), "--inp", "hello"]
     )
     assert "completed success" in stderr, stderr
     assert json.loads(stdout)["out"].strip("\n") == "hello"
@@ -17,7 +17,7 @@ def test_log_dir_echo_output() -> None:
 
 def test_log_dir_echo_no_output() -> None:
     _, stdout, stderr = get_main_output(
-        ["--log-dir", "logs", "tests/echo-stdout-log-dir.cwl", "--inp", "hello"]
+        ["--log-dir", "logs", get_data("tests/echo-stdout-log-dir.cwl"), "--inp", "hello"]
     )
     for dir in os.listdir("logs"):
         for file in os.listdir(f"logs/{dir}"):
