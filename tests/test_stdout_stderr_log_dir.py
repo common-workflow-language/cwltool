12816d3196e4675bc902bc5976bae5f3e993801c
Kevin Hannon <kannon1992@gmail.com>
2022-02-02 10:35:57 -0500

    Update tests/test_stdout_stderr_log_dir.py
    Co-authored-by: Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>

--- tests/test_stdout_stderr_log_dir.py
+++ tests/test_stdout_stderr_log_dir.py
@@ -7,12 +7,14 @@ from cwltool.main import main
 from .util import get_data, get_main_output, needs_docker
 
 
-def test_log_dir_echo_output() -> None:
+def test_log_dir_echo_output(tmp_path: Path) -> None:
     _, stdout, stderr = get_main_output(
-        ["--log-dir", "logs", get_data("tests/echo.cwl"), "--inp", "hello"]
+        ["--log-dir", str(tmp_path), get_data("tests/echo.cwl"), "--inp", "hello"]
     )
     assert "completed success" in stderr, stderr
     assert json.loads(stdout)["out"].strip("\n") == "hello"
+    # and then check in `tmp_path` that there is no stdout log file, since `echo.cwl` uses `stdout` itself.
+    # should there be an empty stderr log, though?
 
 
 def test_log_dir_echo_no_output() -> None:
