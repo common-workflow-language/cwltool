2b9d63732ab36e88bc5ac634c12128ac09813bf0
Tomoya Tanjo <ttanjo@gmail.com>
2022-11-08 18:27:04 +0100

    Add test

--- tests/test_make_template.py
+++ tests/test_make_template.py
@@ -8,3 +8,8 @@ from cwltool import main
 def test_anonymous_record() -> None:
     inputs = cmap([{"type": "record", "fields": []}])
     assert main.generate_example_input(inputs, None) == ({}, "Anonymous record type.")
+
+
+def test_union() -> None:
+    inputs = cmap(["string", "string[]"])
+    assert main.generate_example_input(inputs, None) == ("a_string", 'one of type "string", type "string[]"')
