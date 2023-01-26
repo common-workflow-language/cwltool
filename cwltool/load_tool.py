196f3a6c1749c25cf2ac95ae12ea8ef75fa9416e
Peter Amstutz <peter.amstutz@curii.com>
2023-01-26 11:01:17 -0500

    Pass custom fetcher to fast_parser
    Import flags stored in idx are strings instead of boolean to avoid
    mypy complaints.

--- cwltool/load_tool.py
+++ cwltool/load_tool.py
@@ -33,6 +33,7 @@ from schema_salad.utils import (
     ResolveType,
     json_dumps,
 )
+from schema_salad.fetcher import Fetcher
 
 from . import CWL_CONTENT_TYPES, process, update
 from .context import LoadingContext
@@ -317,8 +318,9 @@ def fast_parser(
     fileuri: Optional[str],
     uri: str,
     loadingContext: LoadingContext,
+        fetcher: Fetcher
 ) -> Tuple[Union[CommentedMap, CommentedSeq], CommentedMap]:
-    lopt = cwl_v1_2.LoadingOptions(idx=loadingContext.codegen_idx, fileuri=fileuri)
+    lopt = cwl_v1_2.LoadingOptions(idx=loadingContext.codegen_idx, fileuri=fileuri, fetcher=fetcher)
 
     if uri not in loadingContext.codegen_idx:
         cwl_v1_2.load_document_with_metadata(
@@ -373,11 +375,11 @@ def fast_parser(
                 ("CommandLineTool", "Workflow", "ExpressionTool"),
                 partial(update_index, loadingContext.loader),
             )
-            loadingContext.loader.idx[flag] = True
+            loadingContext.loader.idx[flag] = flag
             for u in lopt.imports:
-                loadingContext.loader.idx["import:" + u] = True
+                loadingContext.loader.idx["import:" + u] = "import:" + u
             for u in lopt.includes:
-                loadingContext.loader.idx["include:" + u] = True
+                loadingContext.loader.idx["include:" + u] = "include:" + u
 
     return cast(
         Union[CommentedMap, CommentedSeq],
@@ -526,7 +528,7 @@ def resolve_and_validate_document(
         #
         processobj, metadata = document_loader.resolve_ref(uri)
     elif loadingContext.fast_parser:
-        processobj, metadata = fast_parser(workflowobj, fileuri, uri, loadingContext)
+        processobj, metadata = fast_parser(workflowobj, fileuri, uri, loadingContext, document_loader.fetcher)
     else:
         document_loader.resolve_all(workflowobj, fileuri)
         processobj, metadata = document_loader.resolve_ref(uri)
@@ -601,7 +603,7 @@ def make_tool(
         and isinstance(uri, str)
         and not loadingContext.skip_resolve_all
     ):
-        resolveduri, metadata = fast_parser(None, None, uri, loadingContext)
+        resolveduri, metadata = fast_parser(None, None, uri, loadingContext, loadingContext.loader.fetcher)
     else:
         resolveduri, metadata = loadingContext.loader.resolve_ref(uri)
 
