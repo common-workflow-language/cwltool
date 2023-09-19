d3f89d3d90454aafb95c6bafeddcc1e4292ba2b2
Jake Fennick <jake.fennick@axleinfo.com>
2023-09-19 11:23:24 -0600

    Filter out malformed nvidia-smi process_name XML tag

--- cwltool/cuda.py
+++ cwltool/cuda.py
@@ -8,23 +8,47 @@ from .loghandler import _logger
 from .utils import CWLObjectType
 
 
+def cuda_device_count() -> str:
+    """Determine the number of attached CUDA GPUs."""
+    # For the number of GPUs, we can use the following query
+    cmd = ["nvidia-smi", "--query-gpu=count", "--format=csv,noheader"]
+    try:
+        # This is equivalent to subprocess.check_output, but use
+        # subprocess.run so we can use separate MagicMocks in test_cuda.py
+        proc = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)  # nosec
+    except Exception as e:
+        _logger.warning("Error checking number of GPUs with nvidia-smi: %s", e)
+        return "0"
+    return proc.stdout.decode("utf-8")
+
+
 def cuda_version_and_device_count() -> Tuple[str, int]:
     """Determine the CUDA version and number of attached CUDA GPUs."""
+    count = int(cuda_device_count())
+
+    # Since there is no specific query for the cuda version, we have to use
+    # `nvidia-smi -q -x`
+    # However, apparently nvidia-smi is not safe to call concurrently.
+    # With --parallel, sometimes the returned XML will contain
+    # <process_name>\xff...\xff</process_name>
+    # (or other arbitrary bytes) and xml.dom.minidom.parseString will raise
+    # "xml.parsers.expat.ExpatError: not well-formed (invalid token)"
+    # So we either need to use `grep -v process_name` to blacklist that tag,
+    # (and hope that no other tags cause problems in the future)
+    # or better yet use `grep cuda_version` to only grab the tags we will use.
+    cmd = "nvidia-smi -q -x | grep cuda_version"
     try:
-        out = subprocess.check_output(["nvidia-smi", "-q", "-x"])  # nosec
+        out = subprocess.check_output(cmd, shell=True)  # nosec
     except Exception as e:
         _logger.warning("Error checking CUDA version with nvidia-smi: %s", e)
         return ("", 0)
-    dm = xml.dom.minidom.parseString(out)  # nosec
 
-    ag = dm.getElementsByTagName("attached_gpus")
-    if len(ag) < 1 or ag[0].firstChild is None:
-        _logger.warning(
-            "Error checking CUDA version with nvidia-smi. Missing 'attached_gpus' or it is empty.: %s",
-            out,
-        )
+    try:
+        dm = xml.dom.minidom.parseString(out)  # nosec
+    except xml.parsers.expat.ExpatError as e:
+        _logger.warning("Error parsing XML stdout of nvidia-smi: %s", e)
+        _logger.warning("stdout: %s", out)
         return ("", 0)
-    ag_element = ag[0].firstChild
 
     cv = dm.getElementsByTagName("cuda_version")
     if len(cv) < 1 or cv[0].firstChild is None:
@@ -35,13 +59,10 @@ def cuda_version_and_device_count() -> Tuple[str, int]:
         return ("", 0)
     cv_element = cv[0].firstChild
 
-    if isinstance(cv_element, xml.dom.minidom.Text) and isinstance(
-        ag_element, xml.dom.minidom.Text
-    ):
-        return (cv_element.data, int(ag_element.data))
+    if isinstance(cv_element, xml.dom.minidom.Text):
+        return (cv_element.data, count)
     _logger.warning(
-        "Error checking CUDA version with nvidia-smi. "
-        "Either 'attached_gpus' or 'cuda_version' was not a text node: %s",
+        "Error checking CUDA version with nvidia-smi. 'cuda_version' was not a text node: %s",
         out,
     )
     return ("", 0)
--- tests/wf/nvidia-smi-container.cwl
+++ tests/wf/nvidia-smi-container.cwl
@@ -7,7 +7,7 @@ requirements:
     cudaVersionMin: "1.0"
     cudaComputeCapability: "1.0"
   DockerRequirement:
-    dockerPull: "nvidia/cuda:11.4.2-runtime-ubuntu20.04"
+    dockerPull: "nvidia/cuda:11.4.3-runtime-ubuntu20.04"
 inputs: []
 outputs: []
 # Assume this will exit non-zero (resulting in a failing test case) if
