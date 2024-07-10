c73137b99ae79ba9236c3fe616fc16d1070b0561
Michael R. Crusoe <michael.crusoe@gmail.com>
2024-07-10 20:26:18 +0200

    simplify

--- tests/test_2D.py
+++ tests/test_2D.py
@@ -1,18 +1,20 @@
 from pathlib import Path
-import pytest
+
 from .util import get_data, get_main_output
 
-@pytest.fixture(scope="session")
-def test_output_2d_file_format(tmp_path_factory: pytest.TempPathFactory) -> None:
+
+def test_output_2d_file_format(tmp_path: Path) -> None:
     """A simple test for format tag fix for 2D output arrays."""
 
-    tmp_path: Path = tmp_path_factory.mktemp("tmp")
     # still need to create 'filename.txt' as it is needed in output_2D_file_format.cwl
-    _ = tmp_path / "filename.txt"
+    (tmp_path / "filename.txt").touch()
     commands = [
         "--cachedir",
         str(tmp_path / "foo"),  # just so that the relative path of file works out
-        get_data("tests/output_2D_file_format.cwl")]
+        "--outdir",
+        str(tmp_path / "out"),
+        get_data("tests/output_2D_file_format.cwl"),
+    ]
 
     error_code, _, stderr = get_main_output(commands)
 
