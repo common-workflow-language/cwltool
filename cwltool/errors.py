c450ede45d4b123e038208cfa32c2920d5d94480
Peter Amstutz <peter.amstutz@curii.com>
2024-07-16 18:26:02 -0400

    Lint fix

--- cwltool/errors.py
+++ cwltool/errors.py
@@ -1,3 +1,11 @@
+"""Common errors.
+
+WorkflowException and GraphTargetMissingException are aliased to
+equivalent errors from cwl_utils.errors and re-exported by this module
+to avoid breaking the interface for other code.
+
+"""
+
 # flake8: noqa: F401
 
 from cwl_utils.errors import WorkflowException as WorkflowException
